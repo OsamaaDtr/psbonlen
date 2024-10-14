f34105e4618d752e418f15bc01c2cf71e21fc3e5
OsamaaDtr <osamadetra123@gmail.com>
2024-10-15 06:29:49 +0700

    Create database.php

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php 
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $nama_database= "pendaftaranweb";
+    
+    $db = mysqli_connect($hostname, $username, $password, $nama_database);
+    
+
+?>

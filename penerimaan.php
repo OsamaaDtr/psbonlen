fbaba399cb119f11c2da2d6a12cc2dc72acebbc3
OsamaaDtr <osamadetra123@gmail.com>
2024-10-15 06:26:53 +0700

    Create penerimaan.php

---
+++ penerimaan.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Peserta Didik Baru</title>
+</head>
+<body>
+    <h1>Daftar Peserta Didik Baru Tahun Ajaran 2025/2026</h1>
+</body>
+</html>
